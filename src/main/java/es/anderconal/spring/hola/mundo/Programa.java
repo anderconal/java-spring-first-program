b8eaf01fffd9c6a4ea0d2e71b1a9a2db2bff465b
Ander Conal <ander.conal@gmail.com>
2016-07-22 19:44:15 +0200

    Class Path Contexts
    Changing from FileSystemXmlApplicationContext to
    ClassPathXmlApplicationContext.

--- src/main/java/es/anderconal/spring/hola/mundo/Programa.java
+++ src/main/java/es/anderconal/spring/hola/mundo/Programa.java
@@ -1,16 +1,17 @@
 package es.anderconal.spring.hola.mundo;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Programa {
 	public static void main(String[] args) {
 		
-		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("es/anderconal/spring/hola/mundo/beans/beans.xml");
 		
 		Persona persona = (Persona) context.getBean("persona");
 		persona.hablar();
 		
-		((FileSystemXmlApplicationContext)context).close();
+		((ClassPathXmlApplicationContext)context).close();
 	}
 }
