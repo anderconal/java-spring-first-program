a1a66784e8458281b5a0234d08ed1395749baf38
Ander Conal <ander.conal@gmail.com>
2016-07-22 16:29:09 +0200

    My Java Spring first program
    This is my first little Java Spring program

---
+++ src/main/java/es/anderconal/spring/hola/mundo/Persona.java
@@ -0,0 +1,7 @@
+package es.anderconal.spring.hola.mundo;
+
+public class Persona {
+	public void hablar() {
+		System.out.println("Hola, soy una persona.");
+	}
+}
---
+++ src/main/java/es/anderconal/spring/hola/mundo/Programa.java
@@ -0,0 +1,14 @@
+package es.anderconal.spring.hola.mundo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class Programa {
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+		
+		Persona persona = (Persona) context.getBean("persona");
+		persona.hablar();
+	}
+}
